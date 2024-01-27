9f60cb5539516f39db5eef3934266fc919e4a69b
Vrutesh Mayekar <vruteshpm02@gmail.com>
2024-01-27 13:42:23 +0530

    added routes

---
+++ myapp/src/Components/Button/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Button.css"
+export default function Button() {
+  return (
+    <div>
+      <button class="button-37">
+        Get Started <i class="fa-solid fa-arrow-right"></i>
+      </button>
+    </div>
+  );
+}
